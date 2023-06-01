0e64821133f967f7cbd28fdfc142f9d5ff53049b
Lizard-Ranger <wesley@zaammo.co.za>
2023-06-01 12:37:33 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -28,7 +28,7 @@ fi
 
 # Install Rustdesk (Teamviewer Alternative) - Is Open Source
 wget https://github.com/rustdesk/rustdesk/releases/download/1.1.9/rustdesk-1.1.9-raspberry-armhf.deb
-chmod rustdesk-1.1.9-raspberry-armhf.deb
+chmod +x rustdesk-1.1.9-raspberry-armhf.deb
 sudo dpkg --install rustdesk-1.1.9-raspberry-armhf.deb
 
 # Disable Bluetooth.
@@ -49,7 +49,8 @@ apt install -y google-drive-ocamlfuse
 
 # Create the Directory for the Mapped Drive.
 mkdir /home/pi/frame
-mkdir /home/pi/frame/commands
+chmod +rw /home/pi/frame
+chown pi /home/pi/frame
 
 # Create the Directory for the Picture Frame Scripts.
 mkdir /home/pi/MyGDrive
@@ -67,39 +68,12 @@ echo google-drive-ocamlfuse /home/pi/MyGDrive >> /home/pi/frame/gdconnect.sh
 echo '#!/bin/sh' >> /home/pi/frame/GDrive_sync.sh
 echo rm /home/pi/Pictures/* >> /home/pi/frame/GDrive_sync.sh
 echo rm -rf ~/.local/share/Trash/* >> /home/pi/frame/GDrive_sync.sh
-echo 'rsync -r /home/pi/MyGDrive/mpf/pictures/* /home/pi/Pictures' >> /home/pi/frame/GDrive_sync.sh
+echo 'rsync -r /home/pi/MyGDrive/Picture-Frame/pictures/* /home/pi/Pictures' >> /home/pi/frame/GDrive_sync.sh
 echo sudo shutdown -r now >> /home/pi/frame/GDrive_sync.sh
 
-# Creating of Script (Manual_Sync.py)
-echo import os >> /home/pi/frame/Manual_Sync.py
-echo file_path = '/home/pi/MyGDrive/mpf/to_sync.txt' >> /home/pi/frame/Manual_Sync.py
-echo #
-echo # Read the contents of the file >> /home/pi/frame/Manual_Sync.py
-echo with open(file_path, 'r') as file: >> /home/pi/frame/Manual_Sync.py
-echo     file_contents = file.read() >> /home/pi/frame/Manual_Sync.py
-echo #
-echo '# Check if the file contents are true' >> /home/pi/frame/Manual_Sync.py
-echo if file_contents.strip() == 'true': >> /home/pi/frame/Manual_Sync.py
-echo     '# Change the value to false' >> /home/pi/frame/Manual_Sync.py
-echo     file_contents = file_contents.replace('true', 'false') >> /home/pi/frame/Manual_Sync.py
-echo #
-echo     '# Write the modified contents back to the file' >> /home/pi/frame/Manual_Sync.py
-echo     with open(file_path, 'w') as file: >> /home/pi/frame/Manual_Sync.py
-echo         file.write(file_contents) >> /home/pi/frame/Manual_Sync.py
-echo     print('Value changed from true to false') >> /home/pi/frame/Manual_Sync.py
-echo #
-echo     '# Perform system reboot' >> /home/pi/frame/Manual_Sync.py
-echo     os.system('./home/pi/frame/Force_Google_Sync.sh') >> /home/pi/frame/Manual_Sync.py
-echo else: >> /home/pi/frame/Manual_Sync.py
-echo     print('The file does not contain "true"') >> /home/pi/frame/Manual_Sync.py
-echo     "# Perform actions for other conditions if needed" >> /home/pi/frame/Manual_Sync.py
-
-# Creating of Script (Force_Google_Sync.sh)
-echo '#!/bin/sh' >> /home/pi/frame/Force_Google_Sync.sh
-echo rm /home/pi/frame/commands/* >> /home/pi/frame/Force_Google_Sync.sh
-echo rm -rf ~/.local/share/Trash/* >> /home/pi/frame/Force_Google_Sync.sh
-echo 'rsync -r /home/pi/MyGDrive/mpf/commands/* /home/pi/frame/commands/' >> /home/pi/frame/Force_Google_Sync.sh
-echo py /home/pi/frame/Manual_Sync.py >> /home/pi/frame/Force_Google_Sync.sh
+# Creating of Script (GDrive_sync_update.sh)
+echo '#!/bin/sh' >> /home/pi/frame/GDrive_sync_update.sh
+echo 'rsync -r /home/pi/MyGDrive/Picture-Frame/pictures/* /home/pi/Pictures' >> /home/pi/frame/GDrive_sync_update.sh
 
 #Setting file Permissions
 chmod +rwx /home/pi/frame/feh.sh
@@ -108,21 +82,17 @@ chmod +rwx /home/pi/frame/gdconnect.sh
 chown pi /home/pi/frame/gdconnect.sh
 chmod +rwx /home/pi/frame/GDrive_sync.sh
 chown pi /home/pi/frame/GDrive_sync.sh
-chmod +rwx /home/pi/frame/Force_Google_Sync.sh
-chown pi /home/pi/frame/Force_Google_Sync.sh
-chmod +rwx /home/pi/frame/Manual_Sync.py
-chown pi /home/pi/frame/Manual_Sync.py
 chmod +rw /home/pi/MyGDrive
 chown pi /home/pi/MyGDrive
-chmod +rw /home/pi/frame
-chown pi /home/pi/frame
+chmod +rwx /home/pi/frame/GDrive_sync_update.sh
+chmod +rwx /home/pi/frame/GDrive_sync_update.sh
 
 # Updating Crontab for some auotmation and automatic updating.
 (crontab -u pi -l; echo "* 0 * * 1 shutdown -r now >/dev/null 2>&1" ) | crontab -u pi -
 (crontab -u pi -l; echo "@reboot /home/pi/frame/gdconnect.sh") | crontab -u pi -
 (crontab -u pi -l; echo "@reboot /home/pi/frame/feh.sh") | crontab -u pi -
 (crontab -u pi -l; echo "* 0 * * 5 /home/pi/frame/Gdrive_sync.sh >/dev/null 2>&1") | crontab -u pi -
-(crontab -u pi -l; echo "*/30 * * * * /home/pi/frame/Force_Google_Sync.sh" >/dev/null 2>&1) | crontab -u pi -
+(crontab -u pi -l; echo "0 0 * * 1,3 /home/pi/frame/GDrive_sync_update.sh >/dev/null 2>&1") | crontab -u pi -
 
 google-drive-ocamlfuse /home/pi/MyGDrive
 
